67a701d550d9bbd0bf73b45cc3945499cfda7cc5
sundarnagarajan <sun.nagarajan@gmail.com>
2016-04-28 05:24:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     url="https://github.com/sn6uv/gmpy_cffi",
     download_url="https://github.com/sn6uv/gmpy_cffi/tarball/0.1",
     keywords=["PyPy", "gmp"],
+    install_requires = ['cffi'],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
